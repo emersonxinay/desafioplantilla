dfb72c91990aa3fed7b92bf1c84d30f08ddd8b49
Emerson2019 <xinayespinoza@gmail.com>
2019-08-28 02:35:45 -0400

    subiendo a heroku con el arreglo de sqlite

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -41,9 +41,10 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-fla
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-bg-images', '40x40')
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-css')
+
 Rails.application.config.assets.precompile += %w( *.jpg, *.png )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+ Rails.application.config.assets.precompile += %w( admin.js admin.css )
