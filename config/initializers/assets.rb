df4186a9cde779e4a4a5656a1703712ecb5e1e89
Emerson2019 <xinayespinoza@gmail.com>
2019-08-29 20:28:08 -0400

    agrege los las imagenes y todas las direcciones en asset.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,44 +7,36 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'css')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'fonts')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'js')
-
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
-
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font', 'code')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font', 'licensing')
-
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'fonts')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'less')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'scss')
-
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'ico')
-
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'backgrounds')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'modal')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'screenshots')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'team')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'testimonials')
-
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
-
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-bg-images')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-bg-images', '20x20')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-bg-images', '40x40')
-
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font', 'code')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-css')
 
-Rails.application.config.assets.precompile += %w( *.jpg, *.png )
+# JS assets
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js', 'ultm-css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+
+# Images assets
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'screenshots')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'testimonials')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'team')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'modal')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'backgrounds')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-bg-images', '20x20')
+
+
+
+
+
+
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
- Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( *.js *.css *.png *.jpg)
